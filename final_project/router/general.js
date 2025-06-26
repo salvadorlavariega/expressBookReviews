a721d9294d0c1e83b7efe83ef9b51e8e503c8f62
Salvador Lavariega Castellanos <salvador.lav@gmail.com>
2025-06-26 11:30:13 -0600

    Task_4 finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,11 +28,6 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
-  /**
-   * Hints:
-    1. Obtain all the keys for the ‘books’ object.
-    2. Iterate through the ‘books’ array & check the author matches the one provided in the request parameters.
-   */
   const author = req.params.author;
   const booksByAuthor = Object.values(books).filter((book) =>
     book.author.toLowerCase().includes(author.toLowerCase())
@@ -45,8 +40,15 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter((book) =>
+    book.title.toLowerCase().includes(title.toLowerCase())
+  );
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
