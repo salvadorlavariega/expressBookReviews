0320f1c6db6a3722d56917438171dfcba757e364
Salvador Lavariega Castellanos <salvador.lav@gmail.com>
2025-06-26 12:00:54 -0600

    Task_7 completed

--- final_project/index.js
+++ final_project/index.js
@@ -19,51 +19,23 @@ app.use(
 );
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
+  // Check if user is logged in and has valid access token
+  if (req.session.authorization) {
+    let token = req.session.authorization["accessToken"];
 
-  // Check if username or password is missing
-  if (!username || !password) {
-    return res.status(404).json({ message: "Error logging in" });
-  }
-
-  // Authenticate user
-  if (authenticatedUser(username, password)) {
-    // Generate JWT access token
-    let accessToken = jwt.sign(
-      {
-        data: password,
-      },
-      "access",
-      { expiresIn: 60 * 60 }
-    );
-
-    // Store access token and username in session
-    req.session.authorization = {
-      accessToken,
-      username,
-    };
-    return res.status(200).send("User successfully logged in");
+    // Verify JWT token
+    jwt.verify(token, "access", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next(); // Proceed to the next middleware
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
   } else {
-    return res
-      .status(208)
-      .json({ message: "Invalid Login. Check username and password" });
+    return res.status(403).json({ message: "User not logged in" });
   }
 });
-
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-  // Filter the users array for any user with the same username and password
-  let validusers = users.filter((user) => {
-    return user.username === username && user.password === password;
-  });
-  // Return true if any valid user is found, otherwise false
-  if (validusers.length > 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
 const PORT = 5001;
 
